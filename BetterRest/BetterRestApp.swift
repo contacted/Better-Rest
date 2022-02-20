280626f4c7a7b1ce3cffd30ef79c9322feee87b3
Eugene <contact.duk@gmail.com>
2022-02-20 19:05:00 +0300

    Adding initial source code

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Eugene on 18.12.21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
